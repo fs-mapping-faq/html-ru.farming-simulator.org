13042cc74aea5acbed5ccd93800bd784f5ce9a1e
fs-mapping-faq <46016171+fs-mapping-faq@users.noreply.github.com>
2022-04-12 18:11:20 +0300

    Update index.php

--- index.php
+++ index.php
@@ -18,55 +18,55 @@
 <!-- google ads -->
 <?php include("include/google-ads.php"); ?>
 <h1>PMC Farming Simulator</h1>
-<p>Welcome to PMC Farming Simulator homepage.</p>
+<p>Главная страница PMC Farming Simulator.</p>
 </header>
 
 <section>
-	<h2>Farming Simulator 22</h2>
+	<h2>FS22</h2>
 
 <p>
-<a href="22/pmc-terrains.php" class="button">PMC Terrains</a>
-<a href="22/contracts.php" class="button">Contracts</a>
-<a href="22/equipment-vehicle-prices.php" class="button">Equipment Vehicle Prices</a>
-<a href="22/guide.php" class="button">Guide</a>
-<a href="22/map-making-tutorial.php" class="button">Map Making Tutorial</a>
-<a href="22/maps.php" class="button">Maps</a>
-<a href="22/mods.php" class="button">Mods</a>
-<a href="22/settings.php" class="button">Settings</a>
+<a href="22/pmc-terrains.php" class="button">Карты от PMC</a>
+<a href="22/contracts.php" class="button">Контракты</a>
+<a href="22/equipment-vehicle-prices.php" class="button">Цены обслуживания техники</a>
+<a href="22/guide.php" class="button">Гайды</a>
+<a href="22/map-making-tutorial.php" class="button">Материалы по созданию карт</a>
+<a href="22/maps.php" class="button">Карты</a>
+<a href="22/mods.php" class="button">Моды</a>
+<a href="22/settings.php" class="button">Настройки</a>
 </p>
 
 
-	<h2>Farming Simulator 19</h2>
+	<h2>FS19</h2>
 <p>
 <!-- <a href="19/editing.php" class="button">Editing</a> -->
 <!-- <a href="19/multiplayer.php" class="button">Multiplayer</a> -->
-<a href="19/pmc-terrains.php" class="button">PMC Terrains</a>
-<a href="19/contracts.php" class="button">Contracts</a>
-<a href="19/courseplay.php" class="button">Courseplay</a>
-<a href="19/equipment-vehicle-prices.php" class="button">Equipment Vehicle Prices</a>
-<a href="19/guide.php" class="button">Guide</a>
-<a href="19/install-mods.php" class="button">Install Mods</a>
-<a href="19/map-making-tutorial.php" class="button">Map Making Tutorial</a>
-<a href="19/maps.php" class="button">Maps</a>
-<a href="19/mods.php" class="button">Mods</a>
-<a href="19/news.php" class="button">News</a>
-<a href="19/review.php" class="button">Review</a>
-<a href="19/settings.php" class="button">Settings</a>
+<a href="19/pmc-terrains.php" class="button">Карты от PMC</a>
+<a href="19/contracts.php" class="button">Контракты</a>
+<a href="19/courseplay.php" class="button">Курсплей</a>
+<a href="19/equipment-vehicle-prices.php" class="button">Цены обслуживания техники</a>
+<a href="19/guide.php" class="button">Гайды</a>
+<a href="19/install-mods.php" class="button">Установка модов</a>
+<a href="19/map-making-tutorial.php" class="button">Материалы по созданию карт</a>
+<a href="19/maps.php" class="button">Карты</a>
+<a href="19/mods.php" class="button">Моды</a>
+<a href="19/news.php" class="button">Новости</a>
+<a href="19/review.php" class="button">Обзоры</a>
+<a href="19/settings.php" class="button">Настройки</a>
 <a href="19/stories.php" class="button">Stories</a>
 </p>
 
 
 	<h2>Farming Simulator 17</h2>
 <p>
-<a href="courseplay.php" class="button">Courseplay</a>
-<a href="downloads.php" class="button">Downloads</a>
-<a href="editing.php" class="button">Editing</a>
-<a href="guide.php" class="button">Guide</a>
-<a href="map-making-tutorial.php" class="button">Map Making Tutorial</a>
-<a href="maps.php" class="button">Maps</a>
-<a href="mods.php" class="button">Mods</a>
-<a href="review.php" class="button">Review</a>
-<a href="settings.php" class="button">Settings</a>
+<a href="courseplay.php" class="button">Курсплей</a>
+<a href="downloads.php" class="button">Загрузки</a>
+<a href="editing.php" class="button">Редактирование</a>
+<a href="guide.php" class="button">Гайды</a>
+<a href="map-making-tutorial.php" class="button">Материалы по созданию карт</a>
+<a href="maps.php" class="button">Карты</a>
+<a href="mods.php" class="button">Моды</a>
+<a href="review.php" class="button">Обзоры</a>
+<a href="settings.php" class="button">Настройки</a>
 <a href="stories.php" class="button">Stories</a>
 <a href="user-guide-cornbelt-8km-terrain.php" class="button">User Guide: Cornbelt</a>
 <a href="user-guide-north-west-texas-4km-terrain.php" class="button">User Guide: North West Texas</a>
